c138d7c0530bc895d71b526036857e0d69ac1103
theKnightsOfRohan <shadowsovereign100@gmail.com>
2023-10-12 12:36:05 -0700

    Project setup

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,11 @@
 import processing.core.PApplet;
 
 public class Main extends PApplet {
+    public void settings() {
+        size(500, 500);
+    }
 
+    public static void main(String[] args) {
+        PApplet.main("Main");
+    }
 }
