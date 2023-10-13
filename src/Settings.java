ee07a8ace76cec0706ddbf2b55d10b6e87e73c7a
theKnightsOfRohan <shadowsovereign100@gmail.com>
2023-10-13 13:43:47 -0700

    Simplify file structure

--- src/Settings.java
+++ src/Settings.java
@@ -1,5 +1,30 @@
+import java.util.List;
+import processing.core.PApplet;
+
 public class Settings {
     public static final int SCREEN_WIDTH = 500;
     public static final int SCREEN_HEIGHT = 500;
     public static final int GRAVITY = 1;
+}
+
+interface Drawable {
+    public void draw(PApplet app);
+
+    public int getX();
+
+    public int getY();
+
+    public int getWidth();
+
+    public int getHeight();
+}
+
+interface Moveable {
+    public void move();
+}
+
+interface Collidable {
+    public boolean collidesWith(List<Collidable> others);
+
+    public void onCollision();
 }
